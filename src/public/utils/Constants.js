2592050a2bca1fb6d4ef1802ae44f701e2f67f5d
unknown <jinminetics@gmail.com>
2020-01-04 14:59:50 -0800

    env batch

---
+++ src/public/utils/Constants.js
@@ -0,0 +1,42 @@
+export const SITE_TRADE_MARK = "GoodyTray"
+export const LOGO_ADDR = "dist/public/logo.png"
+
+export const PORT = 8080
+export const PORT_SSL = 4433
+export const SERVER_HOST = "http://dev.domain.com"
+export const SERVER_ADDR = SERVER_HOST + ":" + PORT
+
+export const ERROR_NET_UNKNOWN = "An unexpected error occurred. Please make sure you're connected to the internet"
+export const ERROR_DB_OP = "An unexpected error occurred from the server"
+export const LOGIN_SPAN_IN_SECONDS = 86400 * 7
+export const SITE_NAME = "GoodyTray"
+export const SITE_DOT_COM = "goodytray.com"
+export const API_ROOT = "/api/v1/"
+export const MAX_PRODUCT_PHOTOS_SIZE = 1024 * 1024 * 20
+export const DISTINCNT_MESSAGES_PER_HOUR = 5
+export const REVIEWS_PER_PAGE = 30
+export const PRODUCTS_PER_PAGE = 20
+export const NO_PROFILE_PHOTO_IMAGE = "/public/res/images/static/no-profile-photo.jpg"
+export const MAX_ONLINE_INDICATOR_IN_MINS = 5
+export const SITE_TITLE = "Number 1 Place For Cheap Quality Goods"
+export const FACEBOOK_PAGE_NAME = "GoodyTray - " + SITE_TITLE
+export const FACEBOOK_PAGE_LINK = "https://facebook.com/goodytray"
+export const PRODUCTS_PHOTOS_SERVER_DIR = "dist/public/res/images/products"
+export const PRODUCTS_PHOTOS_CLIENT_DIR = "/public/res/images/products/"
+export const USERS_PHOTOS_SERVER_DIR = "dist/public/res/images/users"
+export const USERS_PHOTOS_CLIENT_DIR = "/public/res/images/users/"
+export const STATIC_IMAGES_CLIENT_DIR = "/public/res/images/static/"
+export const PAID_AD_NAME = "Boosted ad"
+
+export const DB_HOST = process.env.DB_HOST
+export const DB_USER = process.env.DB_USER
+export const DB_PASS = process.env.DB_PASS
+
+export const SSL_KEY = process.env.SSL_KEY
+export const SSL_CHAIN = process.env.SSL_CHAIN
+export const SSL_DH = process.env.SSL_DH
+export const SSL_CA = process.env.SSL_CA
+
+export const SSL_SQL_KEY = process.env.SSL_SQL_KEY
+export const SSL_SQL_CHAIN = process.env.SSL_SQL_CHAIN
+export const SSL_SQL_CA = process.env.SSL_SQL_CA
